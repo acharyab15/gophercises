f0a1116fe28f5d7ec3759690976e36fc39c83997
Bipeen <acharyab@seas.upenn.edu>
2018-12-07 08:29:37 -0800

    ex3: Add a default template and http handler

--- cyoa/story.go
+++ cyoa/story.go
@@ -2,8 +2,51 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Choose Your Own Adventure</title>
+	</head>
+	<body>
+		<h1> {{.Title}}</h1>
+		{{range .Paragraphs}}
+			<p>{{.}}</p>
+		{{end}}
+		<ul>
+			{{range .Options}}
+			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+		</ul>
+	</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 // JSONStory takes in a io.Reader as an input and decodes it into a Story
 func JSONStory(r io.Reader) (Story, error) {

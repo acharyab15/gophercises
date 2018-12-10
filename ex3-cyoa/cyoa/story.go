3de8f679b4c851c6687e7bed14b0802c70403c90
Bipeen <acharyab@seas.upenn.edu>
2018-12-10 07:04:40 -0800

    ex3: Custom paths for urls

--- ex3-cyoa/cyoa/story.go
+++ ex3-cyoa/cyoa/story.go
@@ -87,9 +87,16 @@ func WithTemplate(t *template.Template) HandlerOption {
 	}
 }
 
+// WithPathFunc takes in a function and returns a HandlerOption
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
 // NewHandler takes in a Story and HandlerOptions and returns a http.Handler
 func NewHandler(s Story, opts ...HandlerOption) http.Handler {
-	h := handler{s, tpl}
+	h := handler{s, tpl, defaultPathFn}
 	for _, opt := range opts {
 		opt(&h)
 	}
@@ -97,19 +104,24 @@ func NewHandler(s Story, opts ...HandlerOption) http.Handler {
 }
 
 type handler struct {
-	s Story
-	t *template.Template
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
 
 	// removing the leading slash
-	path = path[1:]
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	path := h.pathFn(r)
 
 	if chapter, ok := h.s[path]; ok {
 		err := h.t.Execute(w, chapter)

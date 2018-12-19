1a55576fe341f89c892f1f4415d48b970591bb42
Bipeen <acharyab@seas.upenn.edu>
2018-12-18 20:35:39 -0800

    ex5: filtering urls, refactoring

--- ex5-sitemap-builder/sitemap/cmd/map/main.go
+++ ex5-sitemap-builder/sitemap/cmd/map/main.go
@@ -1,36 +1,82 @@
 package main
 
 import (
-	"encoding/xml"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
 
 	"github.com/acharyab/gophercises/ex4-html-parser/link"
-	"github.com/acharyab/gophercises/ex5-sitemap-builder/sitemap"
 )
 
+/*
+1. GET the webpage
+2. parse all the links on the page (link package)
+3. build proper urls with links
+4. filter out any links w/ a diff domain
+5. find all the pages (BFS)
+6. print out XML
+*/
+
 func main() {
-	urlName := flag.String("url", "", "a url to build a sitemap for")
+	urlFlag := flag.String("url", "https://www.acharyabipeen.com", "a url to build a sitemap for")
 	flag.Parse()
 
-	fmt.Println("The URL is: ", *urlName)
-	resp, err := http.Get(*urlName)
+	fmt.Println("The URL is: ", *urlFlag)
+
+	pages := get(*urlFlag)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
-
 	defer resp.Body.Close()
 
-	links, err := link.Parse(resp.Body)
-
-	urls, err := sitemap.GetURLFromLinks(links)
-
-	output, err := xml.MarshalIndent(urls, "  ", "    ")
-	if err != nil {
-		fmt.Printf("error: %v\n", err)
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
 	}
-	os.Stdout.Write(output)
+	base := baseURL.String()
+
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func hrefs(body io.Reader, base string) []string {
+	links, _ := link.Parse(body)
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
 
 }

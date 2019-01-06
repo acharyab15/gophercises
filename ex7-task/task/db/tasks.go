eefa7646afb3bd6778d145629aabf1c10448ef85
Bipeen <acharyab@seas.upenn.edu>
2019-01-06 11:57:28 -0800

    ex7: AllTasks function to return all the tasks in the db

--- ex7-task/task/db/tasks.go
+++ ex7-task/task/db/tasks.go
@@ -52,6 +52,26 @@ func CreateTask(task string) (int, error) {
 	return id, nil
 }
 
+// AllTasks returns all the tasks from the db
+func AllTasks() ([]Task, error) {
+	var tasks []Task
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			tasks = append(tasks, Task{
+				Key:   btoi(k),
+				Value: string(v),
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
 // Convert integers to byte slices
 func itob(v int) []byte {
 	b := make([]byte, 8)

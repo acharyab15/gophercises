18df63bd26004df98c48e6c15810786b2ca8d92f
Bipeen <acharyab@seas.upenn.edu>
2019-01-06 12:46:47 -0800

    ex7: Update the add, do and list commands

--- ex7-task/task/cmd/add.go
+++ ex7-task/task/cmd/add.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/acharyab/gophercises/ex7-task/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +13,12 @@ var addCmd = &cobra.Command{
 	Short: "Adds a task to your task list.",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
-		fmt.Printf("Added \"%s\" to your task list.\n", task)
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("something went wrong: ", err)
+		} else {
+			fmt.Printf("Added \"%s\" to your task list.\n", task)
+		}
 	},
 }
 

d9b14f5475bffe85de625a1985bc19dfd8dbb849
Bipeen <acharyab@seas.upenn.edu>
2019-02-21 19:56:54 -0800

    ex8: Phone number database. postgres connection

--- ex8-phone-number-normalizer/main.go
+++ ex8-phone-number-normalizer/main.go
@@ -1,11 +1,53 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "acharyab"
+	password = ""
+	dbname   = "gophercises_phone"
+)
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s sslmode=disable", host, port, user)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+	err = resetDB(db, dbname)
+	must(err)
+	db.Close()
 
-	number := "123-456-7890"
-	normalize(number)
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+	must(db.Ping())
+}
+
+// not recommended always
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	must(err)
+	return nil
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	must(err)
+	return createDB(db, name)
 }
 
 func normalize(phone string) string {

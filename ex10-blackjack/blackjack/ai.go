63248d944f3b70e7f939114ccf544533ecd1a8fa
Bipeen <acharyab@seas.upenn.edu>
2019-03-09 10:28:55 -0800

    ex10: AI Interface

---
+++ ex10-blackjack/blackjack/ai.go
@@ -0,0 +1,59 @@
+package blackjack
+
+import (
+	"fmt"
+
+	deck "github.com/acharyab/gophercises/ex9-deck-of-cards"
+)
+
+type AI interface {
+	Bet() int
+	Results(hand [][]deck.Card, dealer []deck.Card)
+	Play(hand []deck.Card, dealer deck.Card) Move
+}
+
+type HumanAI struct{}
+
+func (ai *HumanAI) Bet() int {
+	return 1
+}
+
+func (ai *HumanAI) Play(hand []deck.Card, dealer deck.Card) Move {
+	for {
+		fmt.Println("Player:", hand)
+		fmt.Println("Dealer:", dealer)
+		fmt.Println("What will you do? (h)it, (s)tand")
+		var input string
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			return Hit
+		case "s":
+			return Stand
+		default:
+			fmt.Println("Invalid option: ", input)
+		}
+	}
+}
+
+func (ai *HumanAI) Results(hand [][]deck.Card, dealer []deck.Card) {
+	fmt.Println("==FINAL HANDS==")
+	fmt.Println("Player:", hand)
+	fmt.Println("Dealer:", dealer)
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+type Move func(GameState) GameState
+
+type GameState struct{}
+
+func Hit(gs GameState) GameState {
+	return gs
+}
+
+func Stand(gs GameState) GameState {
+	return gs
+}

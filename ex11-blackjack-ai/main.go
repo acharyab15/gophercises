d5ec97ea88ee43a1a8ceb9ed2ebabbf7e98c04cb
Bipeen <acharyab@seas.upenn.edu>
2019-03-22 06:47:15 -0700

    ex11: Basic AI

--- ex11-blackjack-ai/main.go
+++ ex11-blackjack-ai/main.go
@@ -4,15 +4,49 @@ import (
 	"fmt"
 
 	"github.com/acharyab/gophercises/ex11-blackjack-ai/blackjack"
+	deck "github.com/acharyab/gophercises/ex9-deck-of-cards"
 )
+
+type basicAI struct{}
+
+func (ai *basicAI) Bet(shuffled bool) int {
+	return 100
+}
+
+func (ai *basicAI) Results(hands [][]deck.Card, dealer []deck.Card) {
+	// noop
+}
+
+func (ai *basicAI) Play(hand []deck.Card, dealer deck.Card) blackjack.Move {
+	score := blackjack.Score(hand...)
+	if len(hand) == 2 {
+		if hand[0] == hand[1] {
+			cardScore := blackjack.Score(hand[0])
+			if cardScore >= 8 && cardScore != 10 {
+				return blackjack.MoveSplit
+			}
+		}
+		if (score == 10 || score == 11) && !blackjack.Soft(hand...) {
+			return blackjack.MoveDouble
+		}
+	}
+	dScore := blackjack.Score(dealer)
+	if dScore >= 5 && dScore <= 6 {
+		return blackjack.MoveStand
+	}
+	if score < 13 {
+		return blackjack.MoveHit
+	}
+	return blackjack.MoveStand
+}
 
 func main() {
 	opts := blackjack.Options{
-		Decks:           3,
-		Hands:           2,
+		Decks:           4,
+		Hands:           50000,
 		BlackjackPayout: 1.5,
 	}
 	game := blackjack.New(opts)
-	winnings := game.Play(blackjack.HumanAI())
+	winnings := game.Play(&basicAI{})
 	fmt.Println(winnings)
 }

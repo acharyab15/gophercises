9fbd58fb4feaf3fd0e683f8f62366cab602317ba
Bipeen <acharyab@seas.upenn.edu>
2019-03-28 20:19:06 -0700

    ex12: Finished listing and renaming files non-recursively

--- ex12-file-renaming-tool/main.go
+++ ex12-file-renaming-tool/main.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	fileName := "bi_rthday_001.txt"
-	// => Birthday - 1 of 4.txt
-	newName, err := match(fileName, 4)
+	// fileName := "bi_rthday_001.txt"
+	// // => Birthday - 1 of 4.txt
+	// newName, err := match(fileName, 4)
+	// if err != nil {
+	// 	fmt.Println("no match")
+	// 	os.Exit(1)
+	// }
+	// fmt.Println(newName)
+	dir := "./sample"
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		fmt.Println("no match")
-		os.Exit(1)
+		panic(err)
 	}
-	fmt.Println(newName)
+	count := 0
+	var toRename []string
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Println("Dir: ", file.Name())
+		} else {
+			_, err := match(file.Name(), 4)
+			if err == nil {
+				count++
+				toRename = append(toRename, file.Name())
+			}
+		}
+	}
+	for _, origFilename := range toRename {
+		origPath := filepath.Join(dir, origFilename)
+		newFilename, err := match(origFilename, count)
+		if err != nil {
+			panic(err)
+		}
+		newPath := filepath.Join(dir, newFilename)
+		fmt.Printf("mv %s => %s \n", origPath, newPath)
+		err = os.Rename(origPath, newPath)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 // match returns the new file name, or an error if the file name

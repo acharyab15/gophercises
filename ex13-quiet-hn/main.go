464f9f91b50e5681152f00d8ddf56d1d717d4194
Bipeen <acharyab@seas.upenn.edu>
2019-04-01 20:01:59 -0700

    ex13: Get exactly 30 stories

--- ex13-quiet-hn/main.go
+++ ex13-quiet-hn/main.go
@@ -56,6 +56,19 @@ func getTopStories(numStories int) ([]item, error) {
 	if err != nil {
 		return nil, errors.New("Failed to load top stories")
 	}
+	var stories []item
+	at := 0
+	for len(stories) < numStories {
+		// get a little bit more than numStories
+		need := (numStories - len(stories)) * 5 / 4
+		stories = append(stories, getStories(ids[at:at+need])...)
+		at += need
+	}
+	return stories[:numStories], nil
+}
+
+func getStories(ids []int) []item {
+	var client hn.Client
 	type result struct {
 		idx  int
 		item item
@@ -63,7 +76,7 @@ func getTopStories(numStories int) ([]item, error) {
 	}
 
 	resultCh := make(chan result)
-	for i := 0; i < numStories; i++ {
+	for i := 0; i < len(ids); i++ {
 		go func(idx, id int) {
 			hnItem, err := client.GetItem(id)
 			if err != nil {
@@ -74,7 +87,7 @@ func getTopStories(numStories int) ([]item, error) {
 	}
 
 	var results []result
-	for i := 0; i < numStories; i++ {
+	for i := 0; i < len(ids); i++ {
 		results = append(results, <-resultCh)
 	}
 	sort.Slice(results, func(i, j int) bool {
@@ -89,7 +102,7 @@ func getTopStories(numStories int) ([]item, error) {
 			stories = append(stories, res.item)
 		}
 	}
-	return stories, nil
+	return stories
 }
 
 func isStoryLink(item item) bool {

90f253a927abbf6e3cb8de7cbf39e12837c5110e
Bipeen Acharya <acharyab@seas.upenn.edu>
2019-07-01 17:11:31 -0700

    ex18: Making choices between images

--- ex18-image-transform-service/transform/main.go
+++ ex18-image-transform-service/transform/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"text/template"
 
 	"github.com/acharyab/gophercises/ex18-image-transform-service/transform/primitive"
 )
@@ -25,53 +26,162 @@ func main() {
 			</body></html>`
 		fmt.Fprint(w, html)
 	})
+	mux.HandleFunc("/modify/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("./img/" + filepath.Base(r.URL.Path))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer f.Close()
+		ext := filepath.Ext(f.Name())[1:]
+		modeStr := r.FormValue("mode")
+		if modeStr == "" {
+			// Render mode choices
+			renderModeChoices(w, r, f, ext)
+			return
+		}
+		mode, err := strconv.Atoi(modeStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		//Render num shapes choices
+		nStr := r.FormValue("n")
+		if nStr == "" {
+			renderNumShapeChoices(w, r, f, ext, primitive.Mode(mode))
+			return
+		}
+		numShapes, err := strconv.Atoi(nStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		_ = numShapes
+		http.Redirect(w, r, "/img/"+filepath.Base(f.Name()), http.StatusFound)
+
+	})
 	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		file, header, err := r.FormFile("image")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 		defer file.Close()
 		ext := filepath.Ext(header.Filename)[1:]
-		a, err := genImage(file, ext, 33, primitive.ModeCircle)
+		onDisk, err := tempfile("", ext)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
 		}
-		file.Seek(0, 0)
-		b, err := genImage(file, ext, 33, primitive.ModeEllipse)
+		defer onDisk.Close()
+		_, err = io.Copy(onDisk, file)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			return
 		}
-		file.Seek(0, 0)
-		c, err := genImage(file, ext, 33, primitive.ModePolygon)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		file.Seek(0, 0)
-		d, err := genImage(file, ext, 33, primitive.ModeCombo)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		html := `<html><body>
-		{{range .}}
-		<img src="{{.}}">
-		{{end}}
-		</body></html>`
-		tpl := template.Must(template.New("").Parse(html))
-		images := []string{a, b, c, d}
-		tpl.Execute(w, images)
+		http.Redirect(w, r, "/modify/"+filepath.Base(onDisk.Name()), http.StatusFound)
+
 	})
 	fs := http.FileServer(http.Dir("./img/"))
 	mux.Handle("/img/", http.StripPrefix("/img", fs))
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
 
-func tempfile(prefix, ext string) (*os.File, error) {
-	tmp, err := ioutil.TempFile("./img/", prefix)
-	if err != nil {
-		return nil, errors.New("main: failed to create temporary file")
+func renderNumShapeChoices(w http.ResponseWriter, r *http.Request, rs io.ReadSeeker, ext string, mode primitive.Mode) {
+	opts := []genOpts{
+		{N: 10, M: mode},
+		{N: 20, M: mode},
+		{N: 30, M: mode},
+		{N: 40, M: mode},
 	}
-	defer os.Remove(tmp.Name())
-	return os.Create(fmt.Sprintf("%s.%s", tmp.Name(), ext))
+	imgs, err := genImages(rs, ext, opts...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
+	}
+	html := `<html><body>
+		{{range .}}
+		  <a href="/modify/{{.Name}}?mode={{.Mode}}&n={{.NumShapes}}">
+			<img style="width: 25%;" src="/img/{{.Name}}">
+		  </a>
+		{{end}}
+		</body></html>`
+	tpl := template.Must(template.New("").Parse(html))
+	type dataStruct struct {
+		Name      string
+		Mode      primitive.Mode
+		NumShapes int
+	}
+	var data []dataStruct
+	for i, img := range imgs {
+		data = append(data, dataStruct{
+			Name:      filepath.Base(img),
+			Mode:      opts[i].M,
+			NumShapes: opts[i].N,
+		})
+	}
+
+	err = tpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
+
+}
+func renderModeChoices(w http.ResponseWriter, r *http.Request, rs io.ReadSeeker, ext string) {
+	opts := []genOpts{
+		{N: 10, M: primitive.ModeCircle},
+		{N: 10, M: primitive.ModeBeziers},
+		{N: 10, M: primitive.ModePolygon},
+		{N: 10, M: primitive.ModeCombo},
+	}
+	imgs, err := genImages(rs, ext, opts...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		panic(err)
+	}
+	html := `<html><body>
+		{{range .}}
+		  <a href="/modify/{{.Name}}?mode={{.Mode}}">
+			<img style="width: 25%;" src="/img/{{.Name}}">
+		  </a>
+		{{end}}
+		</body></html>`
+	tpl := template.Must(template.New("").Parse(html))
+	type dataStruct struct {
+		Name string
+		Mode primitive.Mode
+	}
+	var data []dataStruct
+	for i, img := range imgs {
+		data = append(data, dataStruct{
+			Name: filepath.Base(img),
+			Mode: opts[i].M,
+		})
+	}
+
+	err = tpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+type genOpts struct {
+	N int
+	M primitive.Mode
+}
+
+func genImages(rs io.ReadSeeker, ext string, opts ...genOpts) ([]string, error) {
+	var ret []string
+	for _, opt := range opts {
+		rs.Seek(0, 0)
+		f, err := genImage(rs, ext, opt.N, opt.M)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, f)
+	}
+	return ret, nil
 }
 
 func genImage(r io.Reader, ext string, numShapes int, mode primitive.Mode) (string, error) {
@@ -86,4 +196,13 @@ func genImage(r io.Reader, ext string, numShapes int, mode primitive.Mode) (stri
 	defer outFile.Close()
 	io.Copy(outFile, out)
 	return outFile.Name(), nil
+}
+
+func tempfile(prefix, ext string) (*os.File, error) {
+	tmp, err := ioutil.TempFile("./img/", prefix)
+	if err != nil {
+		return nil, errors.New("main: failed to create temporary file")
+	}
+	defer os.Remove(tmp.Name())
+	return os.Create(fmt.Sprintf("%s.%s", tmp.Name(), ext))
 }
